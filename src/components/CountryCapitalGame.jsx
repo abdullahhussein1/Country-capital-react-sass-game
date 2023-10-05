045fec7ff6096d1897a3b2630a9608d6c27dad25
Abdullah Hussein <abdullabale11@gmail.com>
2023-10-05 09:22:52 +0300

    init commit

--- src/components/CountryCapitalGame.jsx
+++ src/components/CountryCapitalGame.jsx
@@ -7,7 +7,6 @@ export default function CountryCapitalGame({ data }) {
   const [pairIsWrong, setPairIsWrong] = useState(false);
   const [firstChoice, setFirstChoice] = useState("");
   const secondChoice = useRef("");
-  // const onButtonClick = (choice, buttonIsClicked) => {
 
   const allCountryAndCapitals = [
     ...Object.keys(countryData),
